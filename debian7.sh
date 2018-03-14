c4544c7e840466ea372c9b880f5198d20c9260bf
PowerMX <powermx@gmail.com>
2018-03-13 19:16:07 -0600

    Update debian7.sh

--- debian7.sh
+++ debian7.sh
@@ -1,51 +1,51 @@
 #!/bin/bash
 
-# initialisasi var
+
 export DEBIAN_FRONTEND=noninteractive
 OS=`uname -m`;
 MYIP=$(wget -qO- ipv4.icanhazip.com);
 MYIP2="s/xxxxxxxxx/$MYIP/g";
 
-# go to root
+
 cd
 
-# disable ipv6
+
 echo 1 > /proc/sys/net/ipv6/conf/all/disable_ipv6
 sed -i '$ i\echo 1 > /proc/sys/net/ipv6/conf/all/disable_ipv6' /etc/rc.local
 
-# install wget and curl
+
 apt-get update;apt-get -y install wget curl;
 
-# set time GMT +8
+
 ln -fs /usr/share/zoneinfo/America/New_York /etc/localtime
 
-# set locale
+
 sed -i 's/AcceptEnv/#AcceptEnv/g' /etc/ssh/sshd_config
 service ssh restart
 
-# set repo
-wget -O /etc/apt/sources.list "https://bitbucket.org/blackkcatt/autoscript/raw/cdfff898e8b1caa50b6d422b9f2e89cf1e050a80/sources.list.debian7"
+#repositorio
+wget -O /etc/apt/sources.list "https://raw.githubusercontent.com/powermx/autoscript/master/sources.list.debian7"
 wget "http://www.dotdeb.org/dotdeb.gpg"
 wget "http://www.webmin.com/jcameron-key.asc"
 cat dotdeb.gpg | apt-key add -;rm dotdeb.gpg
 cat jcameron-key.asc | apt-key add -;rm jcameron-key.asc
 
-# update
+# actualizar
 apt-get update
 
-# install webserver
+# instalar webserver
 apt-get -y install nginx
 
-# install essential package
+# instalar paquetes esenciales
 apt-get -y install nano iptables dnsutils openvpn screen whois ngrep unzip unrar
 
-# install neofetch
+# instalar neofetch
 echo "deb http://dl.bintray.com/dawidd6/neofetch jessie main" | sudo tee -a /etc/apt/sources.list
 curl -L "https://bintray.com/user/downloadSubjectPublicKey?username=bintray" -o Release-neofetch.key && sudo apt-key add Release-neofetch.key && rm Release-neofetch.key
 apt-get update
 apt-get install -y neofetch
 
-# install figlet
+# instalar figlet
 apt-get -y install figlet
 
 echo "clear" >> .bashrc
@@ -63,17 +63,17 @@ echo 'echo -e "Bienvenido!"' >> .bashrc
 echo 'echo -e "Teclee menu para ver el listado de comandos."' >> .bashrc
 echo 'echo -e ""' >> .bashrc
 
-# install webserver
+# instalar webserver
 cd
 rm /etc/nginx/sites-enabled/default
 rm /etc/nginx/sites-available/default
-wget -O /etc/nginx/nginx.conf "https://bitbucket.org/blackkcatt/autoscript/raw/cdfff898e8b1caa50b6d422b9f2e89cf1e050a80/nginx.conf"
+wget -O /etc/nginx/nginx.conf "https://raw.githubusercontent.com/powermx/autoscript/master/nginx.conf"
 mkdir -p /home/vps/public_html
 echo "<pre>1 + 1 = 2</pre>" > /home/vps/public_html/index.html
-wget -O /etc/nginx/conf.d/vps.conf "https://bitbucket.org/blackkcatt/autoscript/raw/cdfff898e8b1caa50b6d422b9f2e89cf1e050a80/vps.conf"
+wget -O /etc/nginx/conf.d/vps.conf "https://raw.githubusercontent.com/powermx/autoscript/master/vps.conf"
 service nginx restart
 
-# blockir torrent
+# block torrent
 iptables -A OUTPUT -p tcp --dport 6881:6889 -j DROP
 iptables -A OUTPUT -p udp --dport 1024:65534 -j DROP
 iptables -A FORWARD -m string --string "get_peers" --algo bm -j DROP
@@ -88,32 +88,32 @@ iptables -A FORWARD -m string --algo bm --string "torrent" -j DROP
 iptables -A FORWARD -m string --algo bm --string "announce" -j DROP
 iptables -A FORWARD -m string --algo bm --string "info_hash" -j DROP
 
-# install openvpn
-wget -O /etc/openvpn/openvpn.tar "https://bitbucket.org/blackkcatt/autoscript/raw/cdfff898e8b1caa50b6d422b9f2e89cf1e050a80/openvpn-debian.tar"
+# instalar openvpn
+wget -O /etc/openvpn/openvpn.tar "https://raw.githubusercontent.com/powermx/autoscript/master/openvpn-debian.tar"
 cd /etc/openvpn/
 tar xf openvpn.tar
 rm -f /etc/openvpn/openvpn.tar
-wget -O /etc/openvpn/1194.conf "https://bitbucket.org/blackkcatt/autoscript/raw/cdfff898e8b1caa50b6d422b9f2e89cf1e050a80/1194.conf"
+wget -O /etc/openvpn/1194.conf "https://raw.githubusercontent.com/powermx/autoscript/master/1194.conf"
 service openvpn restart
 sysctl -w net.ipv4.ip_forward=1
 sed -i 's/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/g' /etc/sysctl.conf
 iptables -t nat -I POSTROUTING -s 192.168.100.0/24 -o eth0 -j MASQUERADE
 iptables-save > /etc/iptables_baru.conf
-wget -O /etc/network/if-up.d/iptables "https://bitbucket.org/blackkcatt/autoscript/raw/cdfff898e8b1caa50b6d422b9f2e89cf1e050a80/iptables"
+wget -O /etc/network/if-up.d/iptables "https://raw.githubusercontent.com/powermx/autoscript/master/iptables"
 chmod +x /etc/network/if-up.d/iptables
 service openvpn restart
 
-# konfigurasi openvpn
+# configuracion openvpn
 cd /etc/openvpn/
-wget -O /etc/openvpn/client.ovpn "https://bitbucket.org/blackkcatt/autoscript/raw/cdfff898e8b1caa50b6d422b9f2e89cf1e050a80/client-1194.conf"
+wget -O /etc/openvpn/client.ovpn "https://raw.githubusercontent.com/powermx/autoscript/master/client-1194.conf"
 sed -i $MYIP2 /etc/openvpn/client.ovpn;
 cp client.ovpn /home/vps/public_html/
 
-# install badvpn
+# instalar badvpn
 cd
-wget -O /usr/bin/badvpn-udpgw "https://bitbucket.org/blackkcatt/autoscript/raw/cdfff898e8b1caa50b6d422b9f2e89cf1e050a80/badvpn-udpgw"
+wget -O /usr/bin/badvpn-udpgw "https://raw.githubusercontent.com/powermx/autoscript/master/badvpn-udpgw"
 if [ "$OS" == "x86_64" ]; then
-  wget -O /usr/bin/badvpn-udpgw "https://bitbucket.org/blackkcatt/autoscript/raw/cdfff898e8b1caa50b6d422b9f2e89cf1e050a80/badvpn-udpgw64"
+  wget -O /usr/bin/badvpn-udpgw "https://raw.githubusercontent.com/powermx/autoscript/master/badvpn-udpgw64"
 fi
 sed -i '$ i\screen -AmdS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7300' /etc/rc.local
 chmod +x /usr/bin/badvpn-udpgw
@@ -125,7 +125,7 @@ sed -i 's/Port 22/Port 22/g' /etc/ssh/sshd_config
 sed -i '/Port 22/a Port 143' /etc/ssh/sshd_config
 service ssh restart
 
-# install dropbear
+# instalar dropbear
 apt-get -y install dropbear
 sed -i 's/NO_START=1/NO_START=0/g' /etc/default/dropbear
 sed -i 's/DROPBEAR_PORT=22/DROPBEAR_PORT=443/g' /etc/default/dropbear
@@ -135,26 +135,26 @@ echo "/usr/sbin/nologin" >> /etc/shells
 service ssh restart
 service dropbear restart
 
-# install stunnel4
+# instalar stunnel4
 apt-get -y install stunnel4
-wget -O /etc/stunnel/stunnel.pem "https://bitbucket.org/blackkcatt/autoscript/raw/cdfff898e8b1caa50b6d422b9f2e89cf1e050a80/stunnel.pem"
-wget -O /etc/stunnel/stunnel.conf "https://bitbucket.org/blackkcatt/autoscript/raw/cdfff898e8b1caa50b6d422b9f2e89cf1e050a80/stunnel.conf"
+wget -O /etc/stunnel/stunnel.pem "https://raw.githubusercontent.com/powermx/autoscript/master/stunnel.pem"
+wget -O /etc/stunnel/stunnel.conf "https://raw.githubusercontent.com/powermx/autoscript/master/stunnel.conf"
 sed -i $MYIP2 /etc/stunnel/stunnel.conf
 sed -i 's/ENABLED=0/ENABLED=1/g' /etc/default/stunnel4
 service stunnel4 restart
 
-# install fail2ban
+# instalar fail2ban
 apt-get -y install fail2ban;
 service fail2ban restart
 
-# install squid3
+# instalar squid3
 cd
 apt-get -y install squid3
-wget -O /etc/squid3/squid.conf "https://bitbucket.org/blackkcatt/autoscript/raw/cdfff898e8b1caa50b6d422b9f2e89cf1e050a80/squid3.conf"
+wget -O /etc/squid3/squid.conf "https://raw.githubusercontent.com/powermx/autoscript/master/squid3.conf"
 sed -i $MYIP2 /etc/squid3/squid.conf;
 service squid3 restart
 
-# install webmin
+# instalar webmin
 cd
 wget -O webmin_1.870_all.deb "http://prdownloads.sourceforge.net/webadmin/webmin_1.870_all.deb"
 dpkg -i --force-all webmin_1.870_all.deb;
@@ -163,36 +163,36 @@ sed -i 's/ssl=1/ssl=0/g' /etc/webmin/miniserv.conf
 rm /root/webmin_1.870_all.deb
 service webmin restart
 
-# install ddos deflate
+# instalar ddos deflate
 cd
 apt-get -y install dnsutils dsniff
-wget https://bitbucket.org/blackkcatt/autoscript/raw/cdfff898e8b1caa50b6d422b9f2e89cf1e050a80/ddos-deflate-master.zip
+wget https://raw.githubusercontent.com/powermx/autoscript/master/ddos-deflate-master.zip
 unzip ddos-deflate-master.zip
 cd ddos-deflate-master
 ./install.sh
 rm -rf /root/ddos-deflate-master.zip
 
-# setting banner
+# banner
 rm /etc/issue.net
-wget -O /etc/issue.net "https://bitbucket.org/blackkcatt/autoscript/raw/cdfff898e8b1caa50b6d422b9f2e89cf1e050a80/issue.net"
+wget -O /etc/issue.net "https://raw.githubusercontent.com/powermx/autoscript/master/issue.net"
 sed -i 's@#Banner@Banner@g' /etc/ssh/sshd_config
 sed -i 's@DROPBEAR_BANNER=""@DROPBEAR_BANNER="/etc/issue.net"@g' /etc/default/dropbear
 service ssh restart
 service dropbear restart
 
-# download script
+# descargar script
 cd /usr/bin
-wget -O menu "https://bitbucket.org/blackkcatt/autoscript/raw/cdfff898e8b1caa50b6d422b9f2e89cf1e050a80/menu.sh"
-wget -O nuevo "https://bitbucket.org/blackkcatt/autoscript/raw/cdfff898e8b1caa50b6d422b9f2e89cf1e050a80/nuevo.sh"
-wget -O prueba "https://bitbucket.org/blackkcatt/autoscript/raw/cdfff898e8b1caa50b6d422b9f2e89cf1e050a80/prueba.sh"
-wget -O borrar "https://bitbucket.org/blackkcatt/autoscript/raw/cdfff898e8b1caa50b6d422b9f2e89cf1e050a80/borrar.sh"
-wget -O checar "https://bitbucket.org/blackkcatt/autoscript/raw/cdfff898e8b1caa50b6d422b9f2e89cf1e050a80/checar.sh"
-wget -O lista "https://bitbucket.org/blackkcatt/autoscript/raw/cdfff898e8b1caa50b6d422b9f2e89cf1e050a80/lista.sh"
-wget -O expirado "https://bitbucket.org/blackkcatt/autoscript/raw/cdfff898e8b1caa50b6d422b9f2e89cf1e050a80/expirado.sh"
-wget -O restart "https://bitbucket.org/blackkcatt/autoscript/raw/cdfff898e8b1caa50b6d422b9f2e89cf1e050a80/restart.sh"
-wget -O speedtest "https://bitbucket.org/blackkcatt/autoscript/raw/cdfff898e8b1caa50b6d422b9f2e89cf1e050a80/speedtest_cli.py"
-wget -O info "https://bitbucket.org/blackkcatt/autoscript/raw/cdfff898e8b1caa50b6d422b9f2e89cf1e050a80/info.sh"
-wget -O acerca "https://bitbucket.org/blackkcatt/autoscript/raw/cdfff898e8b1caa50b6d422b9f2e89cf1e050a80/acerca.sh"
+wget -O menu "https://raw.githubusercontent.com/powermx/autoscript/master/menu.sh"
+wget -O nuevo "https://raw.githubusercontent.com/powermx/autoscript/master/nuevo.sh"
+wget -O prueba "https://raw.githubusercontent.com/powermx/autoscript/master/prueba.sh"
+wget -O borrar "https://raw.githubusercontent.com/powermx/autoscript/master/borrar.sh"
+wget -O checar "https://raw.githubusercontent.com/powermx/autoscript/master/checar.sh"
+wget -O lista "https://raw.githubusercontent.com/powermx/autoscript/master/lista.sh"
+wget -O expirado "https://raw.githubusercontent.com/powermx/autoscript/master/expirado.sh"
+wget -O restart "https://raw.githubusercontent.com/powermx/autoscript/master/restart.sh"
+wget -O speedtest "https://raw.githubusercontent.com/powermx/autoscript/master/speedtest_cli.py"
+wget -O info "https://raw.githubusercontent.com/powermx/autoscript/master/info.sh"
+wget -O acerca "https://raw.githubusercontent.com/powermx/autoscript/master/acerca.sh"
 
 echo "0 0 * * * root /sbin/reboot" > /etc/cron.d/reboot
 
@@ -208,7 +208,7 @@ chmod +x info
 chmod +x expirado
 chmod +x acerca
 
-# finishing
+# finalizando
 cd
 chown -R www-data:www-data /home/vps/public_html
 service nginx start
